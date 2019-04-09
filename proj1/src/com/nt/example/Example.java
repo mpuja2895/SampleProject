3a7878de9373b1fae677de0363a1e27519f3626b
poojaTL <TL@gmail.com>
2019-04-09 12:12:37 +0530

    name()created by tl

---
+++ proj1/src/com/nt/example/Example.java
@@ -0,0 +1,16 @@
+package com.nt.example;
+
+public class Example {
+	 Example(){
+			
+			System.out.println("0-param constructor...........");
+		 }
+	public static void main(String[] args) {
+        name();
+	}
+	
+	public static String name(){
+		return "Example.Example()";
+	
+}
+}
